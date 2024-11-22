b5557598615b82a7bb3b24f28bdbf60b445c545b
Felix Biego <45796740+fbiego@users.noreply.github.com>
2024-11-22 16:12:40 +0300

    update scripts

--- support/esp32_build_extra.py
+++ support/esp32_build_extra.py
@@ -1,19 +1,22 @@
 import glob, os, shutil, fileinput
 import datetime
 
+
+sep = os.sep
+
 Import("env", "projenv")
 print("Post build scripts > copy_elf.py")
 
 
 def after_upload(source, target, env):
-    env_name = str(source[0]).split("/")[-2]
+    env_name = str(source[0]).split(sep)[-2]
     print(f"Copying file from action {target[0]} for {env_name}")
 
-    dest_dir = f"test/esp32"
+    dest_dir = f"test{sep}esp32"
     os.makedirs(dest_dir, exist_ok=True)
 
-    shutil.copyfile(f".pio/build/{env_name}/firmware.elf", f"test/esp32/{env_name}.elf")
-    shutil.copyfile(f".pio/build/{env_name}/firmware.bin", f"test/esp32/{env_name}.bin")
+    shutil.copyfile(f".pio{sep}build{sep}{env_name}{sep}firmware.elf", f"test{sep}esp32{sep}{env_name}.elf")
+    shutil.copyfile(f".pio{sep}build{sep}{env_name}{sep}firmware.bin", f"test{sep}esp32{sep}{env_name}.bin")
     e = datetime.datetime.now()
     print (e.strftime("%H:%M:%S %d-%m-%Y"))
     
--- support/sdl2_build_extra.py
+++ support/sdl2_build_extra.py
@@ -1,6 +1,9 @@
 import glob, os, shutil, fileinput
 import datetime
 
+
+sep = os.sep
+
 Import("env", "projenv")
 
 for e in [ env, projenv ]:
@@ -28,14 +31,14 @@ env.AddTarget(
 
 def after_upload(source, target, env):
 
-    env_prog = str(source[0]).split("/")[-1]
-    env_name = str(source[0]).split("/")[-2]
+    env_prog = str(source[0]).split(sep)[-1]
+    env_name = str(source[0]).split(sep)[-2]
     print(f"Copying file from action {target[0]} for {env_name}")
 
-    dest_dir = f"test/{env_name}"
+    dest_dir = f"test{sep}{env_name}"
     os.makedirs(dest_dir, exist_ok=True)
 
-    shutil.copyfile(f".pio/build/{env_name}/{env_prog}", f"test/{env_name}/{env_prog}")
+    shutil.copyfile(f".pio{sep}build{sep}{env_name}{sep}{env_prog}", f"test{sep}{env_name}{sep}{env_prog}")
     e = datetime.datetime.now()
     print (e.strftime("%H:%M:%S %d-%m-%Y"))
     
